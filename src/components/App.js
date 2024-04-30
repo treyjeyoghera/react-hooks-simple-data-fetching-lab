549775a595e561b0f9f879387d18bc2aca16733f
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-04-30 21:34:17 +0300

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,38 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dog, setDog] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setDog(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return <img src={dog} alt="A Random Dog" />;
+}
+
+export default App;
